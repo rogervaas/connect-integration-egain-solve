a9b5c4f8f11c24e62c3c4e448fdbb85ad4411d26
egaingithubdeveloper <githubdeveloper@egain.com>
2018-03-06 11:12:47 -0800

    updated the python script

--- functions/source/integration-test/main.py
+++ functions/source/integration-test/main.py
@@ -1,22 +1,31 @@
 import cfnresponse
 import traceback
-from botocore.vendored import requests
+from invokecb import invokeCaseBase
 
 
 def handler(event, context):
     try:
         if event['RequestType'] == 'Create':
             # Test Integration
-            print 'Getting all pets'
-            response = requests.get(event['ResourceProperties']['IntegrationEndpoint'])
-            print "Status code: " + str(response.status_code)
-            if response.status_code != 200:
-                raise Exception('Error: Status code received is not 200')
+            print('test eGain AI Integration')
+            base_url = event['ResourceProperties']['BASE_URL']
+            portal = event['ResourceProperties']['PORTAL_ID']
+            casebase = event['ResourceProperties']['CASEBASE_ID']
+            stored_data = 'null'
+            ivr_stage = 'MAIN'
+
+            json_data = {}
+            json_data = invokeCaseBase(base_url, portal, casebase, ivr_stage, stored_data)
+            print("response data: ")
+            print(json_data)
+            if "storedData" not in json_data or \
+                    'X-egain-session' not in json_data["storedData"]:
+                raise Exception('Error: failed to get successful response')
         elif event['RequestType'] == 'Update':
             pass
         elif event['RequestType'] == 'Delete':
             pass
         cfnresponse.send(event, context, cfnresponse.SUCCESS, {}, '')
     except:
-        print traceback.print_exc()
+        print(traceback.print_exc())
         cfnresponse.send(event, context, cfnresponse.FAILED, {}, '')
